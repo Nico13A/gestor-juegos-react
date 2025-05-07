71afcf585dfc6db1b24b9e8284ce1d34a9be9941
Nico13A <nicoantinao1998@gmail.com>
2025-05-07 14:11:35 -0300

    Modifica const

--- src/const/api.js
+++ src/const/api.js
@@ -1,13 +1,13 @@
-export const API_KEY = "c963779517dc4a59a567edf619ae95cb";
+const API_KEY = import.meta.env.VITE_RAWG_API_KEY;
 
-export const API_BASE_URL = "https://api.rawg.io/api";
+const API_BASE_URL = "https://api.rawg.io/api";
 
-export const API_URL_GAMES = `${API_BASE_URL}/games?key=${API_KEY}`;
+const API_URL_GAMES = `${API_BASE_URL}/games?key=${API_KEY}`;
 
 export const getGameByIdUrl = (id) => `${API_BASE_URL}/games/${id}?key=${API_KEY}`;
 
 export const getGamesUrl = (page = 1, search = "", exact = false) => {
-    let url = `${API_BASE_URL}/games?key=${API_KEY}&page=${page}&page_size=18`;
+    let url = API_URL_GAMES + `&page=${page}&page_size=18`;
     if (search) {
         url += `&search=${encodeURIComponent(search)}`;
         if (exact) {
